c6a59566fc1ad4e3510e22e6aa64a1beec6ad5a7
Vivek Subedi <shree665@gmail.com>
2015-02-07 13:00:12 -0500

    added more

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -2,26 +2,70 @@ import java.util.Iterator;
 
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-	 public RandomizedQueue() {
-		 // construct an empty randomized queue
+	//local variables
+	private Item[] queue;
+	private int n = 0;
+	
+	// construct an empty randomized queue
+	 @SuppressWarnings("unchecked")
+	public RandomizedQueue() {
+		 queue = (Item[]) new Object[1];
 	 }
+	 
+	// is the queue empty?
 	 public boolean isEmpty() {
-		return false;
-		 // is the queue empty?
+		return n == 0;
+		 
 	 }
+	 
+	// return the number of items on the queue
 	 public int size() {
-		return 0;
-		 // return the number of items on the queue
+		return n;
 	 }
+	 
+	 // add the item
 	 public void enqueue(Item item) {
-		 // add the item
+		checkItem(item);
+		if(n == queue.length) {
+			resize(2 * queue.length);
+		}
+		queue[n++] = item;
 	 }
+	 
+	 @SuppressWarnings("unchecked")
+	private void resize(int max) {
+		//move stack to a new array of size max
+		Item[] temp = (Item[]) new Object[max];
+		for (int i = 0; i < n; i++) {
+			temp[i] = queue[i];
+		}
+		queue = temp;
+	}
+
+	private void checkItem(Item item) {
+		if (item == null) {
+			throw new NullPointerException("can't put null vlaue in the queue");
+		}
+	}
+	 
+	// delete and return a random item
 	 public Item dequeue() {
+		 
+		checkBoundaries();
+		
+		 int position;
+		 
 		return null;
-		 // delete and return a random item
+		 
 	 }
-	 public Item sample() {
+	 private void checkBoundaries() {
+		 if(isEmpty()){
+	        throw new RuntimeException("THE QUEUE IS EMPTY");  
+	     }
+		 
+	}
+
+	public Item sample() {
 		return null;
 		 // return (but do not delete) a random item
 	 }
