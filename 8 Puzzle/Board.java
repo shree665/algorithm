d4e1c25ce58a615f3e419bbd71e9b576673b1181
Vivek Subedi <shree665@gmail.com>
2015-03-01 14:13:32 -0500

    updated 8 puzzle

--- 8 Puzzle/Board.java
+++ 8 Puzzle/Board.java
@@ -1,52 +1,94 @@
 
 public class Board {
 	
+	private int[][] board;
+	private int n;
+	private int blankRow;
+	private int blankColumn;
+	private int manhatten = 0;
+	
+	/**
+	 * construct a board from an N-by-N array of blocks
+	 * (where blocks[i][j] = block in row i, column j)
+	 * @param blocks
+	 */
 	public Board(int[][] blocks) {
-		// construct a board from an N-by-N array of blocks
+		n = blocks.length;
+		board = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int value = board[i][j] = blocks[i][j];
+				if (value == 0) {
+					blankRow = i;
+					blankColumn = j;
+				} else {
+					int a = (value - 1)/n;
+					int b = (value - 1)%n;
+					manhatten += Math.abs(i - a) + Math.abs(j - b);
+				}
+			}
+		}
 	}
-    // (where blocks[i][j] = block in row i, column j)
-	
+    
+	//board dimension N
 	public int dimension() {
-		return 0;
-		// board dimension N
+		return n;
 	}
 	
+	//number of blocks out of place
 	public int hamming() {
-		return 0;
-		// number of blocks out of place
+		int hamming = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int value = board[i][j];
+				if (value != 0 && value != i * n +j + i + 1) {
+					hamming++;
+				}
+			}
+		}
+		
+		return hamming;
+		
 	}
 	
+	//sum of Manhattan distances between blocks and goal
 	public int manhattan() {
-		return 0;
-		// sum of Manhattan distances between blocks and goal
+		return manhatten;
+		
 	}
 	
+	//is this board the goal board?
 	public boolean isGoal() {
-		return false;
-		// is this board the goal board?
+		return hamming() == 0;
+		
 	}
 	
+	//a board that is obtained by exchanging two adjacent blocks in the same row
 	public Board twin() {
 		return null;
-		// a boadr that is obtained by exchanging two adjacent blocks in the same row
+		
 	}
 	
+	//does this board equal y?
 	public boolean equals(Object y) {
 		return false;
-		// does this board equal y?
+		
 	}
 	
+	//all neighboring boards
 	public Iterable<Board> neighbors() {
 		return null;
-		// all neighboring boards
+		
 	}
 	
+	//string representation of this board (in the output format specified below)
 	public String toString() {
 		return null;
-		// string representation of this board (in the output format specified below)
+		
 	}
 	
+	//unit tests (not graded)
 	public static void main(String[] args) {
-		// unit tests (not graded)
+		
 	}
 }
