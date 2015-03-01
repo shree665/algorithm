4b0004f0b6ae91da1479dae1ea09e9e0265d2db3
Vivek Subedi <shree665@gmail.com>
2015-03-01 16:52:34 -0500

    solver class

--- 8 Puzzle/Board.java
+++ 8 Puzzle/Board.java
@@ -5,7 +5,7 @@ public class Board {
 	private int n;
 	private int blankRow;
 	private int blankColumn;
-	private int manhatten = 0;
+	private int manhattan = 0;
 	
 	/**
 	 * construct a board from an N-by-N array of blocks
@@ -24,7 +24,7 @@ public class Board {
 				} else {
 					int a = (value - 1)/n;
 					int b = (value - 1)%n;
-					manhatten += Math.abs(i - a) + Math.abs(j - b);
+					manhattan += Math.abs(i - a) + Math.abs(j - b);
 				}
 			}
 		}
@@ -53,7 +53,7 @@ public class Board {
 	
 	//sum of Manhattan distances between blocks and goal
 	public int manhattan() {
-		return manhatten;
+		return manhattan;
 		
 	}
 	
@@ -65,26 +65,95 @@ public class Board {
 	
 	//a board that is obtained by exchanging two adjacent blocks in the same row
 	public Board twin() {
-		return null;
+		Board twin;
+        if (board[0][0] != 0 && board[0][1] != 0) {
+            exch(board, 0, 0, 0, 1);
+            twin = new Board(board);
+            exch(board, 0, 0, 0, 1);
+        }
+        else {
+            exch(board, 1, 0, 1, 1);
+            twin = new Board(board);
+            exch(board, 1, 0, 1, 1);
+        }
+        return twin;
 		
 	}
 	
-	//does this board equal y?
+	private void exch(int[][] a, int x1, int y1, int x2, int y2) {
+        int temp = a[x1][y1];
+        a[x1][y1] = a[x2][y2];
+        a[x2][y2] = temp;
+    }
+	
+	//does this board equal board y?
 	public boolean equals(Object y) {
-		return false;
+		if (this == y) {
+			return true;
+		}
 		
+        if (y == null) {
+        	return false;
+        }
+        
+        if (this.getClass() != y.getClass()) {
+        	return false;
+        }
+        
+        Board that = (Board) y;
+        
+        if (that.dimension() != this.dimension()) {
+        	return false;
+        }
+        
+        for (int i = 0; i < n; i++) {
+        	for (int j = 0; j < n; j++) {
+        		if (board[i][j] != that.board[i][j]) {
+        			return false; 
+        		}
+        	}
+        }
+        return true;
 	}
 	
 	//all neighboring boards
 	public Iterable<Board> neighbors() {
-		return null;
-		
+		Stack<Board> neighbors = new Stack<Board>();
+        
+        if (blankRow - 1 > -1) {
+            addNeighbor(blankRow - 1, blankColumn, neighbors);
+        }
+        if (blankRow + 1 < n) { 
+            addNeighbor(blankRow + 1, blankColumn, neighbors);
+        }
+        if (blankColumn - 1 > -1) {
+            addNeighbor(blankRow, blankColumn - 1, neighbors);
+        }
+        if (blankColumn + 1 < n) {
+            addNeighbor(blankRow, blankColumn + 1, neighbors);
+        }
+        
+        return neighbors;
 	}
+	
+	private void addNeighbor(int row, int column, Stack<Board> neighbors) {       
+        exch(board, row, column, blankRow, blankColumn);
+        Board neighbor = new Board(board);
+        exch(board, row, column, blankRow, blankColumn);
+        neighbors.push(neighbor);
+    }
 	
 	//string representation of this board (in the output format specified below)
 	public String toString() {
-		return null;
-		
+		StringBuilder s = new StringBuilder();
+        s.append(n + "\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
 	}
 	
 	//unit tests (not graded)
--- 8 Puzzle/Solver.java
+++ 8 Puzzle/Solver.java
@@ -1,29 +1,149 @@
 
 public class Solver {
 	
-	public Solver(Board initial) {
-		// find a solution to the initial board (using the A* algorithm)
-	}
+	private State finalState;
+    
+    private class State implements Comparable<State> {
+        private Board board;
+        private State previousState;
+        private int costToSolve;
+        private int steps;
+        
+        public State(Board board, State prev) {
+            this.board = board;
+            this.previousState = prev;
+            
+            if (prev == null) {
+            	this.steps = 0;
+            } else {
+            	this.steps = prev.steps + 1;
+            }
+            
+            this.costToSolve = board.manhattan() + this.steps;
+        }
+        
+        public int compareTo(State that) {
+            if (this.costToSolve == that.costToSolve) {
+            	return 0;
+            } else if (this.costToSolve < that.costToSolve) {
+            	return -1;
+            }
+            else {
+            	return 1;
+            }
+        }
+        
+        public boolean isFinal() {
+            return this.board.isGoal();
+        }
+    }
+    
+    /*
+     * find a solution to the initial board (using the A* algorithm)
+     */
+    public Solver(Board initial) {
+    	
+    	//checking for the null values
+    	if (initial == null) {
+			throw new NullPointerException("Can't build board with null values.");
+		}
+    	
+        this.finalState = findSolution(initial);
+    }
+    
+    private State findSolution(Board initial) {
+        MinPQ<State> initialPQ = new MinPQ<State>();
+        MinPQ<State> twinPQ = new MinPQ<State>();
+        
+        State initialState = new State(initial, null);
+        State twinState = new State(initial.twin(), null);
+        
+        while (!initialState.isFinal() && !twinState.isFinal()) {
+                       
+            Iterable<Board> initNeighbs = initialState.board.neighbors();
+            Iterable<Board> twinNeighbs = twinState.board.neighbors();
+            
+            for (Board b: initNeighbs) {
+                if (initialState.previousState == null || !b.equals(initialState.previousState.board)) {
+                	initialPQ.insert(new State(b, initialState));
+                }
+            }
+            
+            for (Board b: twinNeighbs) {
+                if (twinState.previousState == null || !b.equals(twinState.previousState.board)) {
+                	twinPQ.insert(new State(b, twinState));
+                }
+            }
+            
+            initialState = initialPQ.delMin();
+            twinState = twinPQ.delMin();
+        }
+        if (initialState.isFinal()) {
+        	return initialState;
+        }
+        else {
+        	return null;
+        }
+    }
+    
+    /*
+     * is the initial board solvable?
+     */
     public boolean isSolvable() {
-		return false;
-		// is the initial board solvable?
-	}
+        return (this.finalState != null);
+    }
+    
+    /*
+     * minimum number of moves to solve initial board; -1 if no solution
+     */
     public int moves() {
-		return 0;
-		// min number of moves to solve initial board; -1 if unsolvable
-	}
+        if (!isSolvable()) {
+        	return -1;
+        }
+        else return finalState.steps;
+    }
+    
+    /*
+     * sequence of boards in a shortest solution; null if no solution
+     */
     public Iterable<Board> solution() {
-		return null;
-		// sequence of boards in a shortest solution; null if unsolvable
-	}
-    public static void main(String[] args) {
-    	// create initial board from file
+        if (!isSolvable()) return null;
+        
+        Queue<Board> q = new Queue<Board>();
+        restorePath(q, this.finalState);
+        return q;
+    }
+    
+    /*
+     * recursively restore path to solve puzzle
+     */
+    private void restorePath(Queue<Board> q, State s) {
+        if (s == null) {
+        	return;
+        }
+        
+        // recursive call for previous state
+        restorePath(q, s.previousState); 
+        
+        q.enqueue(s.board);
+    }
+    
+    /*
+     * solve a slider puzzle
+     */
+    public static void main(String[] args) {  
+        
+        // create initial board from file
         In in = new In(args[0]);
         int N = in.readInt();
         int[][] blocks = new int[N][N];
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 blocks[i][j] = in.readInt();
+            }
+        }
+        
+        //initial board
         Board initial = new Board(blocks);
 
         // solve the puzzle
@@ -34,8 +154,9 @@ public class Solver {
             StdOut.println("No solution possible");
         else {
             StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
+            for (Board board : solver.solution()) {
                 StdOut.println(board);
+            }
         }
-	}
+    }
 }
