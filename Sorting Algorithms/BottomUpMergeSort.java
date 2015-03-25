c10401af94b975c7bf689f3bc16d69a5037dc762
Vivek Subedi <shree665@gmail.com>
2015-03-24 21:34:09 -0400

    Added bottom up merge sort

---
+++ Sorting Algorithms/BottomUpMergeSort.java
@@ -0,0 +1,49 @@
+
+public class BottomUpMergeSort {
+
+	private static Comparable[] aux;
+	
+	public static void sort (Comparable[] a) {
+		//do lg n passes of pairwise merges
+		int n = a.length;
+		aux = new Comparable[n];
+		for (int i = 1; i < n; i = i + i) { //i = sub array size
+			for (int j = 0; j < n - i; j += i + i) { //j = sub array index
+				merge(a, j, j+i-1, Math.min(j+i+i-1, n-1));
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void merge(Comparable[] a, int lo, int mid, int hi) {
+		int i = lo;
+		int j = mid + 1;
+		
+		for (int k = 0; k <= hi; k++) {
+			aux[k] = a[k];
+		}
+		
+		for (int k = 0; k <= hi; k++) {
+			if (i > mid) {
+				a[k] = aux[j++];
+			} else if (j > hi) {
+				a[k] = aux[i++];
+			} else if (less(aux[j], aux[i])) {
+				a[k] = aux[j++];
+			} else {
+				a[k] = aux[i++];
+			}
+		}
+	}
+	
+	private static boolean less(Comparable<Comparable<?>> v, Comparable<?> w){
+		System.out.println(v.toString() + "  "+w.toString());
+		return v.compareTo(w) < 0;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Sorting Algorithms/TopDownMergeSort.java
@@ -0,0 +1,56 @@
+
+@SuppressWarnings("rawtypes")
+public class TopDownMergeSort {
+	private static Comparable[] aux;
+	
+	public static void sort(Comparable[] a) {
+		aux = new Comparable[a.length];
+		sort(a, 0, a.length-1);
+	}
+	
+	private static void sort(Comparable[] a, int lo, int hi) {
+		//sort a[lo..hi]
+		if (hi <= lo) {
+			return;
+		}
+		
+		int mid = lo + (hi - lo)/2;
+		sort(a, lo, mid); //sort left half
+		sort(a, mid+1, hi); //sort right half
+		merge(a, lo, mid, hi); // merge results
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void merge(Comparable[] a, int lo, int mid, int hi) {
+		int i = lo;
+		int j = mid + 1;
+		
+		for (int k = 0; k <= hi; k++) {
+			aux[k] = a[k];
+		}
+		
+		for (int k = 0; k <= hi; k++) {
+			if (i > mid) {
+				a[k] = aux[j++];
+			} else if (j > hi) {
+				a[k] = aux[i++];
+			} else if (less(aux[j], aux[i])) {
+				a[k] = aux[j++];
+			} else {
+				a[k] = aux[i++];
+			}
+		}
+	}
+	
+	private static boolean less(Comparable<Comparable<?>> v, Comparable<?> w){
+		System.out.println(v.toString() + "  "+w.toString());
+		return v.compareTo(w) < 0;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
